3a98da977a215dd8f9b5c027fd73cd8498b837d1
Dongkwan Kim <todoaskit@gmail.com>
2017-11-07 18:46:12 +0900

    Add test view for base

--- dike/dike/urls.py
+++ dike/dike/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from webdike import views as view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^base/', view.base),
 ]
