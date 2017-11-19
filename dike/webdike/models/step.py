87c0c8af8b53094b0fbec12c28a96796b0cd6c5b
Dongkwan Kim <todoaskit@gmail.com>
2017-11-20 02:08:52 +0900

    Update Step models

--- dike/webdike/models/step.py
+++ dike/webdike/models/step.py
@@ -2,6 +2,9 @@ from django.db import models
 import django.contrib.postgres.fields as pgfields
 
 
+from .sentence import Sentence
+
+
 class Step(models.Model):
     SPLITTED = 'SP'
     POLISHED = 'PO'
@@ -18,7 +21,7 @@ class Step(models.Model):
         max_length=2,
         choices=STAGE_CHOICES,
     )
-    sid = models.IntegerField()
+    sentence = models.ForeignKey(Sentence)
     vote = models.IntegerField(default=0)
     active = models.BooleanField(default=False)
     result = pgfields.JSONField()
