745d78effa0a9ba86b4bb1805d8cb1c0b7877bf8
Dongkwan Kim <todoaskit@gmail.com>
2017-11-23 22:09:36 +0900

    Try natural selection v1

--- dike/webdike/natural_selection.py
+++ dike/webdike/natural_selection.py
@@ -4,6 +4,28 @@ from webdike.models import *
 MAX_POOL = 3
 
 
+def is_same_parent_step(step_1, step_2):
+    """
+    :param step_.*:
+    """
+    p_1 = step_1.parent_step
+    p_2 = step_2.parent_step
+    return (p_1.id == p_2.id)
+
+
+def get_sibling_steps(step_query_set):
+    x = step_query_set[0]
+    y = step_query_set[1]
+    z = step_query_set[2]
+
+    if is_same_parent_step(x, y):
+        return (x, y)
+    elif is_same_parent_step(x, z):
+        return (x, z)
+    else:
+        return (y, z)
+
+
 def need_proliferation(sid):
     """
     Return value is a tuple of boolean value B and a tuple T.
@@ -16,22 +38,44 @@ def need_proliferation(sid):
     """
 
     # Find Sentence from sid
+    sentence = Sentence.objects.get(id=sid)
 
     # Count N, the number of Step instances w/ *next state*
+    current_state = int(sentence.state)
+    next_state = current_state + 1
+    steps = sentence.step_set.filter(stage=next_state)
+    population = len(steps)
 
     # If N >= MAX_POOL:
     #   No need proliferation.
+    if population >= MAX_POOL:
+        need = False
+
     # If N == 2 and Steps (next) have same parent Step (current):
     #   No need proliferation.
     # If N == 2 and Steps (next) have different Step (current):
     #   Need prolifeation.
+    elif population == 2:
+        need = not is_same_parent_step(steps[0], steps[1])
+
     # If N < 2:
     #   Need proliferation.
+    else:
+        need = True
 
     # If No need proliferation:
     #   B = False, T = (Step_next_X, Step_next_Y)
     #   Note that Step_next_X and Step_next_Y have same parent Step (current)
+    if not need:
+        return (need, get_sibling_steps(steps))
+
     # If need proliferation:
     #   B = True, T = (Step_current,)
+    else:
+        step_current = sentence.step_set.filter(stage=current_state, active=True)
+        if step_current:
+            step_current = step_current[0]
+        else:
+            raise Exception("No current step")
+        return (need, (step_current,))
 
-    # TODO How to vote when the current step is After-EXPLAINED
