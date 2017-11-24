db31b71996ce515bc470a6529a414ab5e1998300
Hyeungshik Jung <hyungshike@gmail.com>
2017-11-24 16:08:18 +0900

    Handle saving splitted step

--- dike/webdike/models/step.py
+++ dike/webdike/models/step.py
@@ -1,6 +1,6 @@
 from django.db import models
 import django.contrib.postgres.fields as pgfields
-
+import json
 
 from .sentence import Sentence
 
@@ -24,4 +24,9 @@ class Step(models.Model):
     vote = models.IntegerField(default=0)
     active = models.BooleanField(default=False)
     result = pgfields.ArrayField(models.TextField(), default=[])
-    parent_step = models.ForeignKey('self', blank=True)
+    parent_step = models.ForeignKey('self', null=True)
+
+    def to_json(self):
+        """Return id and result in json format. Used in template"""
+        return json.dumps({
+            'id': self.id, 'result': self.result, 'parent_id': self.parent_step_id })
--- dike/webdike/views.py
+++ dike/webdike/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.http import Http404, JsonResponse
 import json
 from webdike.models import *
 
+
 def base(request):
     return render(request, 'base.html')
+
 
 def home(request):
     judgements = Document.objects.all()
@@ -13,7 +16,8 @@ def home(request):
             "id": j.id,
             "title": j.title
         })
-    return render(request, 'home.html', { 'json_resp': json.dumps(resp) })
+    return render(request, 'home.html', {'json_resp': json.dumps(resp)})
+
 
 def get_judgement(request, jnum):
     doc = Document.objects.get(id=jnum)
@@ -21,9 +25,10 @@ def get_judgement(request, jnum):
     resp = {
         'title': doc.title,
         'desc': doc.description,
-        'sentences': [{'id': s.id, 'text': s.content } for s in sentences]
+        'sentences': [{'id': s.id, 'text': s.content} for s in sentences]
     }
-    return render(request, 'judgement.html', { 'json_resp': json.dumps(resp) })
+    return render(request, 'judgement.html', {'json_resp': json.dumps(resp)})
+
 
 def get_splitter(request, sentence_id):
     sentence = Sentence.objects.get(id=sentence_id)
@@ -31,20 +36,48 @@ def get_splitter(request, sentence_id):
         'id': sentence.id,
         'text': sentence.content,
     }
-    return render(request, 'split.html', { 'json_resp': json.dumps(resp) })
+    return render(request, 'split.html', {'json_resp': json.dumps(resp)})
+
 
 def get_polisher(request, step_id):
+    step_json = Step.objects.get(id=step_id).to_json()
     # TODO use model and split number
-    return render(request, 'polish.html')
+    return render(request, 'polish.html', {'json_resp': step_json})
+
 
 def get_connector(request, step_id):
-    # TODO use model and split number
-    return render(request, 'connect.html')
+    step_json = Step.objects.get(id=step_id).to_json()
+    return render(request, 'connect.html', {'json_resp': step_json})
+
 
 def get_explainer(request, step_id):
-    # TODO use model and split number
+    step_json = Step.objects.get(id=step_id).to_json()
     return render(request, 'explain.html')
+
 
 def get_voter(request, step_id1, step_id2):
     # TODO use model and split numbers
     return render(request, 'vote.html')
+
+
+def save_step(request, stage):
+    payloads = json.loads(request.body)
+    print(payloads)
+
+    parent_step_id = payloads.get("parent_step_id", None)
+    try:
+        result = payloads["result"]
+    except Exception:
+        raise Http404("Failed to parse result")
+    new_step = Step.objects.create(
+        stage=stage,
+        result=result,
+        sentence_id = payloads["sentence_id"]
+    )
+    if parent_step_id:
+        print(parent_step_id)
+        new_step.parent_step_id = parent_step_id
+    new_step.save()
+
+    # TODO Check it's redirecting to correct page
+    return JsonResponse({ "redirect": "/" })
