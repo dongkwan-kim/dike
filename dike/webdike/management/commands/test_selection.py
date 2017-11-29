5fdf347a542c6479ad02dcc87a2b16deeaac5f41
Dongkwan Kim <todoaskit@gmail.com>
2017-11-30 04:40:20 +0900

    Improve test_selection

--- dike/webdike/management/commands/test_selection.py
+++ dike/webdike/management/commands/test_selection.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 import random
+import math
+import pprint
 from ...models import Document, Sentence, Step
 from ...natural_selection import get_work_routing_info, change_populations
 
@@ -7,53 +9,87 @@ class Command(BaseCommand):
     help = ""
 
     def add_arguments(self, parser):
-        parser.add_argument('--docid')
+        parser.add_argument('--docid', action='store_true')
+        parser.add_argument('--users', action='store_true')
         parser.add_argument('--nostop', action='store_true')
         pass
 
+    def get_docid(self, options):
+        if not options['docid']:
+            docid = max([d.id for d in Document.objects.all()])
+        else:
+            docid = options['docid']
+        return docid
+
+    def get_users(self, options):
+        if not options['users']:
+            users = 20
+        else:
+            users = options['users']
+        return users
+
     def handle(self, *args, **options):
 
-        test_steps_start = Document.objects.get(id=options['docid']).last_valid_step_id
+        docid = self.get_docid(options)
+        users = self.get_users(options)
+
+        test_steps_start = Document.objects.get(id=docid).last_valid_step_id
         sample_step_id = test_steps_start
 
         print("deleted steps id larger than {}".format(test_steps_start))
         Step.objects.filter(id__gt=test_steps_start).delete()
-        current_step = Step.objects.get(id=sample_step_id)
-        for i in range(100):
+        initial_step = Step.objects.get(id=sample_step_id)
 
-            print('{}th round'.format(i))
+        ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(math.floor(n / 10) % 10 != 1) * (n % 10 < 4) * n % 10::4])
 
-            next_action = get_work_routing_info(current_step.id)
-            print(next_action)
+        for i in range(users):
 
-            if next_action['creatable'] and current_step.stage is not 4:
-                next_stage = current_step.stage + 1
-                print('\tcreating step at {} stage'.format(next_stage))
+            current_step = initial_step
+            trials = random.randint(1, 5)
+            print('--------------------------------')
+            print('{} user will make {} Steps at maximum'.format(ordinal(i), trials))
 
-                new_step = Step.objects.create(
-                    stage=next_stage,
-                    result=[str(i), str(i)],
-                    sentence_id=current_step.sentence_id,
-                    parent_step=current_step
-                )
-                new_step.save()
-                current_step = new_step
+            for _ in range(trials):
 
-            if next_action['votable']:
-                print('\tvoting')
-                step_list = next_action["step_list"]
-                rest_list = step_list[2:]
-                winner = random.choice(step_list[:2])
-                winner.do_vote()
+                next_action = get_work_routing_info(current_step.id)
+                creatable = next_action['creatable']
+                votable = next_action['votable']
+                pprint.pprint(next_action)
 
-                while len(rest_list) is not 0:
-                    winner = random.choice([winner, rest_list[0]])
-                    rest_list = rest_list[1:]
+                if creatable:
+                    next_stage = current_step.stage + 1
+                    print('\tcreating step at {} stage'.format(next_stage))
 
-            if current_step.stage is 4:
-                current_step = Step.objects.get(id=sample_step_id)
+                    new_step = Step.objects.create(
+                        stage=next_stage,
+                        result=[str(i), str(i)],
+                        sentence_id=current_step.sentence_id,
+                        parent_step=current_step
+                    )
+                    new_step.save()
+                    current_step = new_step
 
-            change_populations(current_step.stage)
+                if votable:
+                    print('\tvoting')
+                    step_list = next_action["step_list"]
+                    rest_list = step_list[2:]
+
+                    # privilege
+                    winner = random.choice([step_list[0]] + step_list[:2])
+                    winner.do_vote()
+
+                    while len(rest_list) > 0:
+                        winner = random.choice([winner, rest_list[0]])
+                        rest_list = rest_list[1:]
+
+                # current step is 4
+                if not creatable and not votable:
+                    # current_step = Step.objects.get(id=sample_step_id)
+                    break
+
+                # Change populations of next generation
+                change_populations(current_step.stage + 1)
+
             if not options['nostop']:
-                input('\tenter any key to proceed')
+                input('\tenter any key to proceed\n')
 
