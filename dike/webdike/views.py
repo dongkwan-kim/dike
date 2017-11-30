cf748a4b4a73a4b64bb2e34d93be828abb6d59aa
Dongkwan Kim <todoaskit@gmail.com>
2017-12-01 04:13:26 +0900

    Adjust voting and natural selection in view

--- dike/webdike/views.py
+++ dike/webdike/views.py
@@ -41,7 +41,6 @@ def get_judgement(request, jnum):
         'desc': doc.description,
         'sentences': [{'id': s.id, 'text': s.content} for s in sentences]
     }
-    print(resp)
     return render(request, 'judgement.html', {'json_resp': json.dumps(resp)})
 
 
@@ -60,20 +59,46 @@ def get_judgement_watch(request, jnum):
     return render(request, 'watch.html', {'json_resp': json.dumps(resp)})
 
 
+def get_route_url_by_natural_select(request, target_step):
+    target_sentence_id = target_step.sentence.id
+    todo = get_work_routing_info(target_step.id)
+    creatable = todo['creatable']
+    votable = todo['votable']
+    next_stage_numeric = int(todo['next_stage'])
+
+    if not creatable and not votable:
+        jnum = target_step.sentence.document.id
+        url = "/judgement/{}".format(jnum)
+        return url
+
+    next_stage = STEP_TO_URL[str(todo['next_stage'])]
+    if next_stage_numeric != int(target_step.stage) + 1:
+        before = next_stage_numeric - 1
+        next_step = Step.objects.filter(stage=before, sentence__id=target_sentence_id).order_by('-population')[0]
+    else:
+        next_step = target_step
+
+    if votable:
+        step_list = todo["step_list"]
+        request.session['step_list'] = [s.id for s in step_list]
+    else:
+        request.session['step_list'] = []
+
+    if creatable:
+        url = '/editor/{}/{}'.format(next_stage, next_step.id)
+        return url
+
+
 @login_required
 def get_sentence(request, sentence_id):
     sentence = Sentence.objects.get(id=sentence_id)
     sentence.add_hit()
 
-    step = Step.objects.filter(stage=0, sentence__id=sentence_id)[0]
-    todo = get_work_routing_info(step.id)
-    if todo['creatable']:
-        return HttpResponseRedirect('/editor/split/{}'.format(step.id))
-    elif todo['votable']:
-        step_list = todo["step_list"]
-        rest_list = step_list[2:]
-        return HttpResponseRedirect('/vote?step1={}&step2={}'
-                                    .format(step_list[0], step_list[1]))
+    init_stage = 0
+
+    init_step = Step.objects.filter(stage=init_stage, sentence__id=sentence_id)[0]
+    url = get_route_url_by_natural_select(request, init_step)
+    return HttpResponseRedirect(url)
 
 
 def get_splitter(request, step_id):
@@ -97,10 +122,22 @@ def get_explainer(request, step_id):
     return render(request, 'explain.html', {'json_resp': json.dumps(parent_step)})
 
 
+def route_winner_or_new_to_editor(request, winner_or_new):
+    change_populations(winner_or_new.stage, winner_or_new.sentence.id)
+    url = get_route_url_by_natural_select(request, winner_or_new)
+    return JsonResponse({"redirect": url})
+
+
+def route_winner_or_new_to_vote(request, winner_or_new):
+    step_list = [winner_or_new.id] + request.session['step_list']
+    rest_list = step_list[2:]
+    request.session['step_list'] = rest_list
+    return JsonResponse({"redirect": '/vote?step1={}&step2={}'.format(step_list[0], step_list[1])})
+
+
 def save_step(request, stage):
     if int(stage) not in range(1, 5):
         raise Http404("Invalid stage number")
-
 
     payloads = json.loads(request.body)
     parent_step_id = payloads.get("parent_step_id", None)
@@ -109,31 +146,21 @@ def save_step(request, stage):
     except Exception:
         print('err!!')
         raise Http404("Failed to parse result")
+
     new_step = Step.objects.create(
         stage=stage,
         result=result,
-        sentence_id = payloads["sentence_id"]
+        sentence_id=payloads["sentence_id"]
     )
     if parent_step_id:
         new_step.parent_step_id = parent_step_id
     new_step.save()
 
-    if int(stage) == 4:
-        return JsonResponse({"redirect": '/'})
-
-    change_populations(new_step.stage)
-    todo = get_work_routing_info(new_step.id)
-    if todo['creatable']:
-        next_step = STEP_TO_URL[str(int(new_step.stage) + 1)]
-        next_url = '/editor/{}/{}'.format(
-            next_step, str(new_step.id))
-    else:  # todo['votable']:
-        step_list = todo["step_list"]
-        rest_list = step_list[2:]
-        next_url = '/vote?step1={}&step2={}&rest={}'.format(
-            step_list[0], step_list[1], rest_list)
-
-    return JsonResponse({"redirect": next_url})
+    rest_list = request.session['step_list']
+    if rest_list:
+        return route_winner_or_new_to_vote(request, new_step)
+    else:
+        return route_winner_or_new_to_editor(request, new_step)
 
 
 def handle_vote(request):
@@ -148,17 +175,20 @@ def get_vote(request):
     step2_id = int(request.GET['step2'])
     step1 = Step.objects.get(id=step1_id).to_dict()
     step2 = Step.objects.get(id=step2_id).to_dict()
-    # TODO use model and split numbers
-    return render(request, 'vote.html', {'step1': step1, 'step2': step2 })
+    return render(request, 'vote.html', {'step1': step1, 'step2': step2})
 
 
 def save_vote(request):
     payloads = json.loads(request.body)
     chosen_step_id = payloads['step_id']
     print(chosen_step_id)
-    Step.objects.get(id=chosen_step_id).do_vote()
-    # TODO Check it's redirecting to correct page
-    return JsonResponse({"redirect": "/"})
+    winner = Step.objects.get(id=chosen_step_id)
+    winner.do_vote()
+
+    try:
+        return route_winner_or_new_to_vote(request, winner)
+    except:
+        return route_winner_or_new_to_editor(request, winner)
 
 
 def get_stats(request, jnum):
